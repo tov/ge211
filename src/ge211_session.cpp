0a4164b5c71d5faca30e62884f3700f2aa5d432d
Jesse A. Tov <jesse.tov@gmail.com>
2018-08-24 03:07:01 -0500

    Starting and stopping audio.

--- src/ge211_session.cpp
+++ src/ge211_session.cpp
@@ -3,6 +3,7 @@
 
 #include <SDL.h>
 #include <SDL_image.h>
+#include <SDL_mixer.h>
 #include <SDL_ttf.h>
 
 #include <clocale>
@@ -14,6 +15,11 @@ namespace detail {
 Session::Session()
 {
     setlocale(LC_ALL, "en_US.utf8");
+
+    int mix_flags = Mix_Init(0);
+    if (mix_flags == 0) {
+        warn_sdl() << "Could not initialize audio mixer";
+    }
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fatal_sdl() << "Could not initialize graphics";
@@ -31,6 +37,14 @@ Session::Session()
         exit(1);
     }
 
+    if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY,
+                      MIX_DEFAULT_FORMAT,
+                      2,
+                      4096) < 0)
+    {
+        warn_sdl() << "Could not open audio channels";
+    }
+
     SDL_StartTextInput();
 }
 
@@ -38,9 +52,12 @@ Session::~Session()
 {
     SDL_StopTextInput();
 
+    Mix_CloseAudio();
+
     TTF_Quit();
     IMG_Quit();
     SDL_Quit();
+    Mix_Quit();
 }
 
 } // end namespace detail
