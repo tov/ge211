ef1ddd36785e1fc8ae1161f82fe3b4458325db8d
Jesse A. Tov <jesse.tov@gmail.com>
2018-08-29 00:33:03 -0500

    Seeding mt19937 with high_resolution_clock in addition to random_device.
    This is to help on Windows, where GCC (MinGW) apparently doesn't support
    random_device.

--- src/ge211_random.cpp
+++ src/ge211_random.cpp
@@ -1,12 +1,19 @@
 #include "ge211_random.h"
 
+#include <chrono>
+
+using namespace std;
+
 namespace ge211 {
 
 static auto construct_engine()
 {
-    std::random_device rd;
-    std::mt19937_64 engine(rd());
-    return std::move(engine);
+    random_device rd;
+    auto time = static_cast<random_device::result_type>(
+            chrono::high_resolution_clock()
+                    .now().time_since_epoch().count());
+    mt19937_64 engine(rd() | time);
+    return move(engine);
 }
 
 Random::Random()
