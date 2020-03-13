661302f5a2b3744c5610a6d632692a8b1478991b
Jesse A. Tov <jesse.tov@gmail.com>
2020-03-12 23:42:53 -0500

    Fix: compile correct resource installation path into library.

--- cmake/Ge211Installer.cmake
+++ cmake/Ge211Installer.cmake
@@ -1,13 +1,22 @@
 include(GNUInstallDirs)
 
-install(TARGETS ge211 utf8-cpp EXPORT Ge211Config
-        ARCHIVE  DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        LIBRARY  DESTINATION ${CMAKE_INSTALL_LIBDIR}
-        RUNTIME  DESTINATION ${CMAKE_INSTALL_BINDIR})
+install(TARGETS         ge211
+                        utf8-cpp
+        EXPORT          Ge211Config
+        ARCHIVE         DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        LIBRARY         DESTINATION ${CMAKE_INSTALL_LIBDIR}
+        RUNTIME         DESTINATION ${CMAKE_INSTALL_BINDIR})
 
-install(DIRECTORY include/ DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
-install(DIRECTORY Resources/ DESTINATION ${CMAKE_INSTALL_DATADIR}/ge211)
+install(DIRECTORY       include/
+        DESTINATION     ${CMAKE_INSTALL_INCLUDEDIR}
+        FILES_MATCHING  PATTERN "*.hxx")
 
-install(EXPORT Ge211Config
-        DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/Ge211)
-export(TARGETS ge211 FILE Ge211Config.cmake)
+install(DIRECTORY       Resources/
+        DESTINATION     ${CMAKE_INSTALL_DATADIR}/ge211)
+
+install(EXPORT          Ge211Config
+        DESTINATION     ${CMAKE_INSTALL_DATADIR}/cmake/Ge211)
+
+export( TARGETS         ge211
+        FILE            Ge211Config.cmake)
+
