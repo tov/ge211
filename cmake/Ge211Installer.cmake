ed33ff12f145d99e0e9834f551330afbc53fb162
Jesse A. Tov <jesse.tov@gmail.com>
2020-05-06 03:13:33 -0500

    Generate and install Ge211ConfigVersion.cmake

--- cmake/Ge211Installer.cmake
+++ cmake/Ge211Installer.cmake
@@ -1,4 +1,10 @@
 include(GNUInstallDirs)
+include(CMakePackageConfigHelpers)
+
+set(config_version_cmake
+        ${CMAKE_CURRENT_BINARY_DIR}/Ge211/Ge211ConfigVersion.cmake)
+set(config_install_dir
+        ${CMAKE_INSTALL_DATADIR}/cmake/Ge211)
 
 install(TARGETS         ge211
                         utf8-cpp
@@ -16,8 +22,15 @@ install(DIRECTORY       Resources/
         DESTINATION     ${CMAKE_INSTALL_DATADIR}/ge211)
 
 install(EXPORT          Ge211Config
-        DESTINATION     ${CMAKE_INSTALL_DATADIR}/cmake/Ge211)
+        DESTINATION     ${config_install_dir})
+
+install(FILES           ${config_version_cmake}
+        DESTINATION     ${config_install_dir})
 
 export( TARGETS         ge211
         FILE            Ge211Config.cmake)
+
+write_basic_package_version_file(${config_version_cmake}
+        VERSION         ${CMAKE_PROJECT_VERSION}
+        COMPATIBILITY   SameMinorVersion)
 
