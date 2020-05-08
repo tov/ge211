36e8b3bf7c538a0f5c03d73657b752ec4b98e648
Jesse A. Tov <jesse.tov@gmail.com>
2020-05-08 00:42:48 -0500

    CMake running Doxygen.

--- cmake/Ge211Docs.cmake
+++ cmake/Ge211Docs.cmake
@@ -1,18 +1,20 @@
-set(DOXYGEN_ALIASES                [=[preconditions=#### Preconditions:\n\n]=])
-set(DOXYGEN_DISABLE_INDEX          YES)
-set(DOXYGEN_EXCLUDE_SYMBOLS        detail)
-set(DOXYGEN_GENERATE_TREEVIEW      YES)
+set(DOXYGEN_OUTPUT_DIRECTORY       doc)
+set(DOXYGEN_JAVADOC_AUTOBRIEF      YES)
+set(DOXYGEN_ALIASES                [=[preconditions=#### Preconditions: ]=])
 set(DOXYGEN_HIDE_FRIEND_COMPOUNDS  YES)
 set(DOXYGEN_HIDE_SCOPE_NAMES       YES)
-set(DOXYGEN_HTML_DYNAMIC_SECTIONS  YES)
-set(DOXYGEN_JAVADOC_AUTOBRIEF      YES)
-set(DOXYGEN_LAYOUT_FILE            doxygen/DoxygenLayout.xml)
-set(DOXYGEN_OUTPUT_DIRECTORY       doc)
 set(DOXYGEN_SHOW_INCLUDE_FILES     NO)
-set(DOXYGEN_SOURCE_BROWSER         YES)
+set(DOXYGEN_LAYOUT_FILE            doxygen/DoxygenLayout.xml)
+set(DOXYGEN_EXCLUDE_SYMBOLS        detail)
 set(DOXYGEN_USE_MDFILE_AS_MAINPAGE doxygen/FRONTMATTER.md)
+set(DOXYGEN_SOURCE_BROWSER         YES)
+set(DOXYGEN_HTML_DYNAMIC_SECTIONS  YES)
+set(DOXYGEN_DISABLE_INDEX          YES)
+set(DOXYGEN_GENERATE_TREEVIEW      YES)
+set(DOXYGEN_TAGFILES
+        "doxygen/cppreference.xml=http://en.cppreference.com/w/")
 
-doxygen_add_docs(ge211_documentation
+doxygen_add_docs(ge211_docs
         doxygen/FRONTMATTER.md
         include/
         src/)
