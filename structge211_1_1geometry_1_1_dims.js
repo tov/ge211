d87022a061b9d2ef810bd4d98e72388c7951c74c
Jesse A. Tov <jesse.tov@gmail.com>
2021-05-14 11:53:23 -0600

    Update documentation

--- structge211_1_1geometry_1_1_dims.js
+++ structge211_1_1geometry_1_1_dims.js
@@ -7,7 +7,9 @@ var structge211_1_1geometry_1_1_dims =
     [ "into", "structge211_1_1geometry_1_1_dims.html#aae76b2d714cddf3dde38ede451e85cd4", null ],
     [ "operator!=", "structge211_1_1geometry_1_1_dims.html#a12b2c3988fbce2a1eb365c24da10c7a3", null ],
     [ "operator*", "structge211_1_1geometry_1_1_dims.html#ad753325a4f80592ee7b87c1fc04b88d8", null ],
+    [ "operator*", "structge211_1_1geometry_1_1_dims.html#afeb061b11f349498cde6652a1afea774", null ],
     [ "operator*=", "structge211_1_1geometry_1_1_dims.html#acdccbb332f23bc2ac22971afb4eced18", null ],
+    [ "operator*=", "structge211_1_1geometry_1_1_dims.html#af022617388b1acea66505efdebe134f5", null ],
     [ "operator+", "structge211_1_1geometry_1_1_dims.html#a148e0d74136d97b574279c75b9a65316", null ],
     [ "operator+=", "structge211_1_1geometry_1_1_dims.html#a7252683f84f317f487dc12761d1f49a3", null ],
     [ "operator-", "structge211_1_1geometry_1_1_dims.html#ab11e0838dedf4fb6ac6b68281c0c2fcc", null ],
@@ -15,7 +17,11 @@ var structge211_1_1geometry_1_1_dims =
     [ "operator-=", "structge211_1_1geometry_1_1_dims.html#a254822d4e756a679aae964839c90ae97", null ],
     [ "operator/", "structge211_1_1geometry_1_1_dims.html#a3913923742713e6079a308d2ed8a9d1e", null ],
     [ "operator/=", "structge211_1_1geometry_1_1_dims.html#aab03f256e6e977a36056624f61d47ec1", null ],
+    [ "operator<", "structge211_1_1geometry_1_1_dims.html#a72ce9721d09e836259958f151e55034b", null ],
+    [ "operator<=", "structge211_1_1geometry_1_1_dims.html#a3ebbf86c335fcf072697f32cb811aa4c", null ],
     [ "operator==", "structge211_1_1geometry_1_1_dims.html#a2a7e53046d84047fc54eefb7bf1ff3b0", null ],
+    [ "operator>", "structge211_1_1geometry_1_1_dims.html#afe048214b8008098dae15c27ec497eb6", null ],
+    [ "operator>=", "structge211_1_1geometry_1_1_dims.html#a207e47d8554e2199feb4680f3e3e6486", null ],
     [ "height", "structge211_1_1geometry_1_1_dims.html#a050576b0776777b293a8aa065f369292", null ],
     [ "width", "structge211_1_1geometry_1_1_dims.html#afacc5a60c416c4e435b3e7b7634e11d9", null ]
 ];
