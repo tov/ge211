8ad51b2c4d654955647b7005b98d9da882f1f5d8
Jesse A. Tov <jesse.tov@gmail.com>
2021-06-09 20:00:33 -0500

    working! fireworks.

--- src/ge211_base.cxx
+++ src/ge211_base.cxx
@@ -4,6 +4,8 @@
 
 #include <SDL.h>
 
+#include <iostream> // XXX TODO FIXME
+
 namespace ge211 {
 
 using namespace detail;
@@ -12,6 +14,18 @@ using namespace detail;
 const Dims<int> Abstract_game::default_window_dimensions{800, 600};
 const char* const Abstract_game::default_window_title = "ge211 window";
 const Color Abstract_game::default_background_color = Color::black();
+
+Abstract_game::Abstract_game()
+{
+    std::cout << "Game " << this << " is here\n";
+}
+
+Abstract_game::~Abstract_game()
+{
+    std::cout << "Game " << this << " out\n";
+    std::cout << "  (engine " << engine_ << ")\n";
+    std::abort();
+}
 
 Dims<int> Abstract_game::initial_window_dimensions() const
 {
@@ -25,7 +39,13 @@ std::string Abstract_game::initial_window_title() const
 
 void Abstract_game::run()
 {
-    Engine(*this).run();
+#ifdef __EMSCRIPTEN__
+    auto& engine = *(new Engine(*this));
+#else
+    auto engine = Engine(*this);
+#endif
+
+    engine.run();
 }
 
 void Abstract_game::quit() NOEXCEPT_
@@ -61,58 +81,6 @@ void Abstract_game::poll_channels_()
 void Abstract_game::on_key_down(Key key)
 {
     if (key.code() == '\u001B') quit();
-}
-
-std::unique_ptr<Abstract_game::Guard_>
-Abstract_game::guard_()
-{
-    return std::make_unique<Guard_>(this);
-}
-
-Abstract_game::Guard_::Guard_(Abstract_game *g)
-        : game(g)
-{
-    start_();
-}
-
-Abstract_game::Guard_::~Guard_()
-{
-    quit_();
-}
-
-Abstract_game::Guard_::Guard_(Abstract_game::Guard_&& other)
-        : game(other.release_())
-{ }
-
-Abstract_game::Guard_&
-Abstract_game::Guard_::operator=(Abstract_game::Guard_&& other)
-{
-    quit_();
-    game = other.release_();
-    return *this;
-}
-
-void
-Abstract_game::Guard_::start_()
-{
-    if (game) {
-        game->on_start();
-    }
-}
-
-void
-Abstract_game::Guard_::quit_()
-{
-    if (game) {
-        game->on_quit();
-        game = nullptr;
-    }
-}
-
-Abstract_game *
-Abstract_game::Guard_::release_()
-{
-    return std::exchange(game, nullptr);
 }
 
 } // end namespace ge211
