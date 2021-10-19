76d103e67faa80925b5fddba038d028baffad2de
Jesse A. Tov <jesse.tov@gmail.com>
2021-10-19 15:43:18 -0500

    Clarify helpers for testing Random_source.

--- test/test_random_helpers.hxx
+++ test/test_random_helpers.hxx
@@ -16,54 +16,59 @@ namespace test_random_helpers {
 template <typename T>
 using Producer = std::function<T()>;
 
+
 /// Harness for calling a function returning type T and checking
 /// the results.
 template <typename T>
 class Checker
 {
 public:
-    Checker(Producer<T> action)
-            : action_(action)
+    Checker(Producer<T> producer)
+            : producer_(producer)
     { }
 
     void okay() const
     {
-        CHECK_NOTHROW(action_());
+        CHECK_NOTHROW(producer_());
     }
 
     void equals(T expected) const
     {
-        CHECK(action_() == expected);
+        CHECK(producer_() == expected);
     }
 
     void is_between(T lo, T hi) const
     {
-        auto value = action_();
+        auto value = producer_();
         CHECK(lo <= value);
         CHECK(value <= hi);
     }
 
     void throws_bounds() const
     {
-        CHECK_THROWS_AS(action_(), ge211::Random_source_bounds_error);
+        CHECK_THROWS_AS(producer_(), ge211::Random_source_bounds_error);
     }
 
     void throws_unsupported() const
     {
-        CHECK_THROWS_AS(action_(), ge211::Random_source_unsupported_error);
+        CHECK_THROWS_AS(producer_(), ge211::Random_source_unsupported_error);
     }
 
 protected:
-    Producer<T> action_;
+    Producer<T> producer_;
 };
 
+
+// A builder is a checker for Random_source with special logic for
+// calling Random_source member functions next, next_between, and
+// next_with_probability.
 template <typename T>
 class Builder : public Checker<ge211::Random_source<T>>
 {
 public:
     using Source = ge211::Random_source<T>;
     using Super = Checker<Source>;
-    using Super::action_;
+    using Super::producer_;
     using Super::okay;
     using Super::throws_bounds;
 
@@ -75,26 +80,28 @@ public:
     Checker<T> next() const
     {
         return {[=] {
-            return action_().next();
+            return producer_().next();
         }};
     }
 
     Checker<T> next_between(T lo, T hi) const
     {
         return {[=] {
-            return action_().next_between(lo, hi);
+            return producer_().next_between(lo, hi);
         }};
     }
 
     Checker<T> next_with_probability(double p) const
     {
         return {[=] {
-            return action_().next_with_probability(p);
+            return producer_().next_with_probability(p);
         }};
     }
 };
 
 
+// Factory function for inferring the instantiation of the Builder
+// template.
 template <typename T, typename... R>
 Builder<T>
 check(R ... args)
